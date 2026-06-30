710cc83d4a395216e64c095e162646088b4a898c
Sergi Rivero Galán <56429173+sriverogalan@users.noreply.github.com>
2026-06-30 08:22:45 +0200

    fix: point DMG URLs to public releases repo

--- Casks/better-screenshoot.rb
+++ Casks/better-screenshoot.rb
@@ -2,12 +2,12 @@ cask "better-screenshoot" do
   version "0.2.1"
 
   on_arm do
-    url "https://github.com/sriverogalan/better-screenshoot/releases/download/v#{version}/Better-Screenshoot_#{version}_aarch64.dmg"
+    url "https://github.com/sriverogalan/better-screenshoot-releases/releases/download/v#{version}/Better-Screenshoot_#{version}_aarch64.dmg"
     sha256 "b3d425c087c2706f858381d3f6e7f8beef4c59c16a7e2006d4dd79bfc042eaa9"
   end
 
   on_intel do
-    url "https://github.com/sriverogalan/better-screenshoot/releases/download/v#{version}/Better-Screenshoot_#{version}_x64.dmg"
+    url "https://github.com/sriverogalan/better-screenshoot-releases/releases/download/v#{version}/Better-Screenshoot_#{version}_x64.dmg"
     sha256 "722105dcbb5578a2f02166906b7842c1673d503586569e655d6512040b835b71"
   end
 
